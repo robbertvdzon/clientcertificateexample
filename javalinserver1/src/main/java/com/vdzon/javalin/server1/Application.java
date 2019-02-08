12280437e080ad3dde396c80f42dc6c7b7f93da2
robbert <robbert@vdzon.com>
2019-02-08 11:02:51 +0100

    add javalin sample

---
+++ javalinserver1/src/main/java/com/vdzon/javalin/server1/Application.java
@@ -0,0 +1,10 @@
+package com.vdzon.javalin.server1;
+
+import io.javalin.Javalin;
+
+public class Application {
+    public static void main(String[] args){
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
