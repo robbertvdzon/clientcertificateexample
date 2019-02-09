3088d173c4432c0f8eaf11ee06eb9a5644038d9e
robbert <robbert@vdzon.com>
2019-02-09 18:47:44 +0100

    add demo apps

--- javalin_json_logging/src/main/java/com/demojsonlog/JavalinJsonLoggingApplication.java
+++ javalin_json_logging/src/main/java/com/demojsonlog/JavalinJsonLoggingApplication.java
@@ -1,8 +1,8 @@
-package com.demo;
+package com.demojsonlog;
 
 import io.javalin.Javalin;
 
-public class Demo1Application {
+public class JavalinJsonLoggingApplication {
     public static void main(String[] args) {
         Javalin app = Javalin.create();
         new RestEndpoints().initRestEndpoints(app);
