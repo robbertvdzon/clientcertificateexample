d991ec8c4f08c0ea507596ab360314197b9b7315
robbert <robbert@vdzon.com>
2019-02-10 20:34:29 +0100

    add client rest call

--- javalin_http_client/src/main/java/com/httpclient/RestEndpoints.java
+++ javalin_http_client/src/main/java/com/httpclient/RestEndpoints.java
@@ -5,44 +5,72 @@ import net.jodah.failsafe.CircuitBreaker;
 import net.jodah.failsafe.Failsafe;
 import net.jodah.failsafe.Fallback;
 import net.jodah.failsafe.RetryPolicy;
+import org.apache.http.client.fluent.Request;
 
-import java.net.ConnectException;
+import java.io.IOException;
 import java.time.Duration;
 
 public class RestEndpoints {
+    private RetryPolicy<Object> retryPolicy;
+    private CircuitBreaker<Object> circuitBreakerPolicy;
+    private Fallback<Object> fallbackPolicy;
+
 
     public void initRestEndpoints(Javalin app) {
+        buildPolicies();
         app.get("/", ctx -> ctx.result("Hello World from javalin"));
         app.get("/json", ctx -> ctx.json(new MyJson("Hello World")));
         app.post("/json", ctx -> ctx.result("Got post of " + ctx.bodyAsClass(MyJson.class).getText()));
         app.get("/external", ctx -> ctx.result(getFromExternalWithRetryAndCircuitBreaker()));
     }
 
-
-    private String getFromExternalWithRetryAndCircuitBreaker(){
-        RetryPolicy<Object> retryPolicy = new RetryPolicy<>()
+    private void buildPolicies() {
+        retryPolicy = new RetryPolicy<>()
                 .handle(RuntimeException.class)
                 .withDelay(Duration.ofSeconds(1))
                 .withMaxRetries(3);
 
-        CircuitBreaker<Object> circuitBreaker = new CircuitBreaker<>();
+        circuitBreakerPolicy = new CircuitBreaker<>()
+                .handleIf((Object exchange, Throwable ex) -> {
+//                    boolean badRequest = exchange != null && StatusCodes.BAD_REQUEST == exchange.getStatusCode();
+//                    return badRequest || ex != null;
+                    return false;
+                })
+                // If 7 out of 10 requests fail Open the circuit
+                .withFailureThreshold(7, 10)
+                // When half open if 3 out of 5 requests succeed close the circuit
+                .withSuccessThreshold(3, 5)
+                // Delay this long before half opening the circuit
+                .withDelay(Duration.ofSeconds(2))
+                .onClose(() -> System.out.println("Circuit Closed"))
+                .onOpen(() -> System.out.println("Circuit Opened"))
+                .onHalfOpen(() -> System.out.println("Circuit Half-Open"));
 
-        Fallback<Object> fallback = Fallback.of(this::getFromExternalFallback);
+        fallbackPolicy = Fallback.of(this::getFromExternalFallback);
+    }
 
-        return Failsafe.with(fallback, retryPolicy, circuitBreaker).get(()->getFromExternal());
+    private String getFromExternalWithRetryAndCircuitBreaker() {
+        return Failsafe.with(fallbackPolicy, retryPolicy, circuitBreakerPolicy).get(this::getFromExternal);
     }
 
     private String getFromExternal() {
-        System.out.println("----> get from ex");
-        if (true) throw new RuntimeException("sample");
-        return "from external";
-
+        try {
+            System.out.println("----> get from ex");
+            return Request
+                    .Get("http://localhost:8080/")
+                    .execute()
+                    .returnContent()
+                    .asString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
+
 
     private String getFromExternalFallback() {
         System.out.println("----> get from fallback");
         return "from fallback";
-
     }
 
 }
